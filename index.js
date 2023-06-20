02132e09b9571ebccb8d525bbf29e4e476c0cf81
Saitbot <73251805+Aza-saitbot@users.noreply.github.com>
2023-06-20 10:49:32 +0300

    work

--- index.js
+++ index.js
@@ -4,34 +4,43 @@ const botToken = '6108666847:AAFwp6d0DJFKQyuj2AFdGNUv00FLnyqIR-0';
 
 const targetAccountId = '999109125';
 
-const groupId = -1001973111412;
+const groupId = -1001810854616;
 
-let keywords = ['Купить недвижимость в Турции', 'Продать недвижимость в Турции', 'Недвижимость в Турции','Недвижимость'
-,'недвижимость возле моря Турция'];
+let keywords = ['Купить недвижимость в Турции', 'Продать недвижимость в Турции', 'Недвижимость в Турции', 'Недвижимость', 'недвижимость возле моря Турция'];
 
 const bot = new TelegramBot(botToken, { polling: true });
 
-// Обработка входящих сообщений группы
 bot.onText(/.*/, (msg) => {
+    let firstName = msg.from?.first_name
+    let lastName = msg.from?.last_name ?? '';
     const chatId = msg.chat.id;
-    const authorName = msg.from.first_name + ' ' + msg.from.last_name;
+    const authorName = firstName + ' ' + lastName
     const messageText = msg.text;
+    const messageTime = new Date(msg.date * 1000).toLocaleString(); // Конвертируем время сообщения
+    const authorId = msg.from.id;
+    const username = msg.from.username;
 
-    // Проверка наличия ключевых слов в сообщении
+    console.log('msg',msg)
+    console.log('authorName, messageText', authorName, messageText);
+
     const foundKeywords = keywords.filter(keyword => new RegExp(keyword, 'i').test(messageText));
     if (foundKeywords.length > 0) {
-        // Отправка информации об авторе и тексте сообщения на аккаунт Telegram
-        const targetMessage = `Найдено ключевое слово в группе:\nАвтор: ${authorName}\nСообщение: ${messageText}`;
-        bot.sendMessage(targetAccountId, targetMessage);
+        const targetMessage = `Найдено ключевое слово в группе:\nАвтор: ${
+            authorName}\nID: ${authorId}\nUsername: ${username}\nСообщение: ${messageText}`;
+        bot.sendMessage(targetAccountId, targetMessage)
+            .then(() => {
+                console.log('Сообщение успешно отправлено на аккаунт Telegram');
+            })
+            .catch(error => {
+                console.log('Ошибка при отправке сообщения на аккаунт Telegram:', error);
+            });
     }
 });
 
-// Запуск мониторинга группы
 bot.on('polling_error', (error) => {
-    console.log('Ошибка при мониторинга группы!!!!:', error);
+    console.log('Ошибка при мониторинге группы:', error);
 });
 
-// Обработка команды добавления ключевого слова
 bot.onText(/\/addkeyword (.+)/, (msg, match) => {
     const keyword = match[1].trim();
     if (keyword) {
@@ -42,7 +51,6 @@ bot.onText(/\/addkeyword (.+)/, (msg, match) => {
     }
 });
 
-// Обработка команды запроса списка ключевых слов
 bot.onText(/\/listkeywords/, (msg) => {
     if (keywords.length > 0) {
         const keywordsList = keywords.join('\n');
@@ -52,8 +60,10 @@ bot.onText(/\/listkeywords/, (msg) => {
     }
 });
 
-bot.getChatMembersCount(groupId).then((count) => {
-    console.log(`Бот запущен в группе с ID ${groupId}, количество участников: ${count}`);
-}).catch((error) => {
-    console.log('Произошла ошибка при получении количества участников группы:', error);
-});
+bot.getChatMemberCount(groupId)
+    .then(count => {
+        console.log(`Бот запущен в группе с ID ${groupId}, количество участников: ${count}`);
+    })
+    .catch(error => {
+        console.log('Произошла ошибка при получении количества участников группы:', error);
+    });
